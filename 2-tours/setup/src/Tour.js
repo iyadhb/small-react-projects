4d3b025b189b844c6a20c5a39bb549e252b2a7bf
Iyad <iyadhb@gmail.com>
2020-11-16 11:08:26 +0000

    Tours - remove and load tour

--- 2-tours/setup/src/Tour.js
+++ 2-tours/setup/src/Tour.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 
-const Tour = ({id, image, info, price, name}) => {
+const Tour = ({id, image, info, price, name, removeTour }) => {
   const [readMore, setReadMore] = useState(false);
+
+ 
 
   return <article className="single-tour">
   <img src={image} alt={name} />
@@ -16,7 +18,7 @@ const Tour = ({id, image, info, price, name}) => {
   {readMore? "show less" : "read more"}
   </button>
   </p>
-  <button className="delete-btn">not interested</button>
+  <button className="delete-btn" onClick={()=> removeTour(id)}>not interested</button>
   </footer>
   </article>;
 };
