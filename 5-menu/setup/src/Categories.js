5a7ddca1901c4c4d09e5af184ec276263bf941b3
Iyad <iyadhb@gmail.com>
2020-11-17 21:29:56 +0000

    Dynamic Categories

--- 5-menu/setup/src/Categories.js
+++ 5-menu/setup/src/Categories.js
@@ -1,14 +1,21 @@
 import React from 'react';
 
-const Categories = ({filterItems}) => {
+const Categories = ({filterItems, categories}) => {
 
 
-  return <div className="btn-container">
-<button className="filter-btn" onClick={()=> filterItems('all')}>all</button>
+  return (
+    <div className="btn-container">
+    {categories.map((category, index)=> {
+return <button type="button" className="filter-btn" key={index}
+onClick={()=> filterItems(category)}
+>
 
-<button className="filter-btn" onClick={()=> filterItems('breakfast')}>breakfast</button>
+{category}
+</button>
+    })}
 
-  </div>;
+  </div>
+  );
 };
 
 export default Categories;
