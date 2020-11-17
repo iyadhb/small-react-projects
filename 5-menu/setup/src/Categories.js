3ff3e22bab436d6a578daeca3996ed2ea9ef61e3
Iyad <iyadhb@gmail.com>
2020-11-17 18:15:05 +0000

    all categories

--- 5-menu/setup/src/Categories.js
+++ 5-menu/setup/src/Categories.js
@@ -4,6 +4,8 @@ const Categories = ({filterItems}) => {
 
 
   return <div className="btn-container">
+<button className="filter-btn" onClick={()=> filterItems('all')}>all</button>
+
 <button className="filter-btn" onClick={()=> filterItems('breakfast')}>breakfast</button>
 
   </div>;
